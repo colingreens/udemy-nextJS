0f7f88afbc5bba22e4ad7d8db2d4da25ecfc9f62
colin_greens <colin.j.benson@gmail.com>
2023-01-08 00:47:24 -0800

    finished events search

--- udemy-nextjs/pages/events/[...slug].js
+++ udemy-nextjs/pages/events/[...slug].js
@@ -1,7 +1,70 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../dummy-data";
+import Button from "../../components/ui/button";
+import EventList from "../../components/events/event-list";
+import ErrorAlert from "../../components/ui/error-alert";
+import ResultsTitle from "../../components/events/results-title";
+
 export default function FilteredEventsPage() {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth > 12 ||
+    numMonth < 1
+  ) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>Invalid Filter Please Adjust Your Values</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/Events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>No events found on selected date.</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const date = new Date(numYear, numMonth - 1);
+
   return (
-    <div>
-      <h1>Filtered Events</h1>
-    </div>
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </Fragment>
   );
 }
